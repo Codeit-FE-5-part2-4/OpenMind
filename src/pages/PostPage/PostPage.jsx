59fc7dbb623878a99b321b2178fd1d1530b9deb5
Bae uijin <baeuijin01@gmail.com>
2024-04-09 10:42:37 +0900

    feat: 포스트페이지 컴포넌트 추가

---
+++ src/pages/PostPage/PostPage.jsx
@@ -0,0 +1,9 @@
+import logoImage from "../../assets/images/logo.png";
+
+export default function PostPage() {
+  return (
+    <div>
+      <img src={logoImage} alt="openmind" />
+    </div>
+  );
+}
