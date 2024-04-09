6c90959e15630a4478a8a979ad720bfd87156235
kuum97 <kuum97@icloud.com>
2024-04-09 15:44:36 +0900

    Rename, Remove: 폴더에 컴포넌트 하나만 들어가도록 수정

---
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -0,0 +1,18 @@
+import speechBubble from "../../assets/images/icon/speech-bubble.svg";
+import testCatProfile from "../../assets/images/test-cat-profile.png";
+
+function QuestionCard() {
+  return (
+    <a href="/">
+      <img src={testCatProfile} alt="프로필 사진" />
+      <h2>이름</h2>
+      <div>
+        <img src={speechBubble} alt="말풍선" />
+        <div>받은 질문</div>
+        <div>9개</div>
+      </div>
+    </a>
+  );
+}
+
+export default QuestionCard;
