1cb483b1556f90dfe03f646b4612e6925d2eaab0
Bae uijin <baeuijin01@gmail.com>
2024-04-09 16:22:18 +0900

    style:퀘스쳔카드 스타일 생성

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,5 +1,6 @@
 import likeIcon from "../../assets/images/icon/likeIcon.svg";
 import dislikeIcon from "../../assets/images/icon/dislikeIcon.svg";
+import styles from "./QuestionCard.module.css";
 
 export default function QuestionCard({ question, AnswererProfile }) {
   const answerStatusMsg = {
@@ -7,31 +8,43 @@ export default function QuestionCard({ question, AnswererProfile }) {
     notAnswered: "미답변",
   };
 
+  let answerStatusStyle = question.answer
+    ? styles.isAnswered
+    : styles.notAnswered;
+  answerStatusStyle += " ";
+  answerStatusStyle += styles.answerStatus;
   return (
-    <div>
-      <span>
+    <div className={styles.questionCard}>
+      <span className={answerStatusStyle}>
         {question.answer
           ? answerStatusMsg.isAnswered
           : answerStatusMsg.notAnswered}
       </span>
-      <div>
-        <span>{question.createdAt}</span>
-        <p>{question.content}</p>
+      <div className={styles.questionInfo}>
+        <span className={styles.createdAt}>{question.createdAt}</span>
+        <p className={styles.questionTitle}>{question.content}</p>
       </div>
-      <div>
-        <img src={AnswererProfile.imageSource} />
-        <div>
-          <span>{AnswererProfile.name}</span>
-          <span>{question.answer.createdAt}</span>
-          <p>{question.answer.content}</p>
+      <div className={styles.answerContainer}>
+        <img
+          className={styles.userProfileImage}
+          src={AnswererProfile.imageSource}
+        />
+        <div className={styles.answer}>
+          <div className={styles.answerInfo}>
+            <span className={styles.answererName}>{AnswererProfile.name}</span>
+            <span className={styles.createdAt}>
+              {question.answer.createdAt}
+            </span>
+          </div>
+          <p className={styles.answerContent}>{question.answer.content}</p>
         </div>
       </div>
-      <div>
-        <div>
+      <div className={styles.judgeAnswerContainer}>
+        <div className={styles.judge}>
           <img src={likeIcon} />
           <span>{`좋아요 ${question.like}`}</span>
         </div>
-        <div>
+        <div className={styles.judge}>
           <img src={dislikeIcon} />
           <span>{`싫어요 ${question.dislike}`}</span>
         </div>
