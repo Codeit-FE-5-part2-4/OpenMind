b9e73a0ea93a40c8cd13086b707a8d308d5e5b33
Kimhansaem <khs_38@naver.com>
2024-04-09 15:58:49 +0900

    Design: main 컴포넌트 스타일 추가

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,7 +1,24 @@
-import "./Main.css";
+import ArrowButton from "../../components/ArrowButton/ArrowButton";
+import BoxButton from "../../components/BoxButton/BoxButton";
+import styles from "./Main.module.css";
+import logoImg from "../../assets/images/logo.png";
+import NameInput from "../../components/NameInput/NameInput";
 
 function Main() {
-    return <div>Main</div>;
+    return (
+        <main className={styles.mainWrap}>
+            <header>
+                <ArrowButton text={"질문하러 가기"} />
+            </header>
+            <h1 className={styles.logo}>
+                <img src={logoImg} alt="" />
+            </h1>
+            <div className={styles.mainConBox}>
+                <NameInput placeholder={"이름을 입력하세요"} />
+                <BoxButton text={"질문 받기"} />
+            </div>
+        </main>
+    );
 }
 
 export default Main;
