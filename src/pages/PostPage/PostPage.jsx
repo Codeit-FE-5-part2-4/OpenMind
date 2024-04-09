ffc18ed749c627256edb4e08c2ec4ebff8fb86f5
Bae uijin <baeuijin01@gmail.com>
2024-04-09 19:38:09 +0900

    feat:퀘스천 모달창 생성

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import logoImage from "../../assets/images/logo.png";
 import PostProfile from "../../components/PostProfile/PostProfile";
 import QuestionList from "../../components/QuestionList/QuestionList";
 import styles from "./PostPage.module.css";
 import { mockProfile, mockQuestions } from "./mockdata";
+import QuestionModal from "../../components/QuestionModal/QuestionModal";
 
 export default function PostPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className={styles.container}>
       <h1>
@@ -13,6 +25,18 @@ export default function PostPage() {
       </h1>
       <PostProfile userProfile={mockProfile} />
       <QuestionList questions={mockQuestions} AnswererProfile={mockProfile} />
+      {showModal && (
+        <>
+          <QuestionModal
+            closeModal={handleModalClose}
+            answererProfile={mockProfile}
+          />
+          <div className={styles.overlay}></div>
+        </>
+      )}
+      <button disabled={showModal} onClick={handleModalOpen}>
+        질문 하러가기
+      </button>
     </div>
   );
 }
