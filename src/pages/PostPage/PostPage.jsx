d49c31101a6ea14c0d4ae419016f1d6715e27297
Bae uijin <baeuijin01@gmail.com>
2024-04-09 12:11:02 +0900

    style : 포스트 페이지 스타일 적용

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,10 +1,14 @@
 import logoImage from "../../assets/images/logo.png";
 import PostProfile from "../../components/PostProfile/PostProfile";
+import styles from "./PostPage.module.css";
 
 export default function PostPage() {
   return (
-    <div>
-      <img src={logoImage} alt="openmind" />
+    <div className={styles.container}>
+      <h1>
+        <span className={styles.blind}>Openmind</span>
+        <img src={logoImage} alt="openmind" />
+      </h1>
       <PostProfile />
     </div>
   );
