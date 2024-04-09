998022b1692913900231282d0af5475473e68d1f
kuum97 <kuum97@icloud.com>
2024-04-09 14:36:17 +0900

    Docs: 질문카드 리스트 마크업 생성

---
+++ src/components/QuestionCardListPage/QuestionCardList.jsx
@@ -0,0 +1,17 @@
+function QuestionCardListPage() {
+  return (
+    <section>
+      <div>
+        <button>정렬</button>
+      </div>
+      <ul>
+        <li>카드 컴포넌트</li>
+      </ul>
+      <ol>
+        <li>페이지네이션</li>
+      </ol>
+    </section>
+  );
+}
+
+export default QuestionCardListPage;
