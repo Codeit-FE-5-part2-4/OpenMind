a9aefe915066b201711778f5b8b9f93faaede496
kuum97 <kuum97@icloud.com>
2024-04-09 17:30:27 +0900

    Add: 페이지 이동 컴포넌트 추가

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,29 @@
+function Pagination() {
+  return (
+    <ol>
+      <li>
+        <button>&lt;</button>
+      </li>
+      <li>
+        <button>1</button>
+      </li>
+      <li>
+        <button>2</button>
+      </li>
+      <li>
+        <button>3</button>
+      </li>
+      <li>
+        <button>4</button>
+      </li>
+      <li>
+        <button>5</button>
+      </li>
+      <li>
+        <button>&gt;</button>
+      </li>
+    </ol>
+  );
+}
+
+export default Pagination;
