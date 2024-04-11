e2bcbe7c2e78931d96bdbc79a8554536c5793ed9
Kimhansaem <khs_38@naver.com>
2024-04-11 16:04:03 +0900

    Feat: 메인 페이지 > MainHeader 컴포넌트 추가

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,15 +1,13 @@
-import ArrowButton from "../../components/ArrowButton/ArrowButton";
 import BoxButton from "../../components/BoxButton/BoxButton";
 import styles from "./Main.module.css";
 import logoImg from "../../assets/images/logo.png";
 import NameInput from "../../components/NameInput/NameInput";
+import MainHeader from "../../components/MainHeader/MainHeader";
 
 function Main() {
     return (
         <main className={styles.mainWrap}>
-            <header>
-                <ArrowButton text={"질문하러 가기"} />
-            </header>
+            <MainHeader />
             <h1 className={styles.logo}>
                 <img src={logoImg} alt="" />
             </h1>
