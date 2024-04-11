9bea620838dd32946648b55c82653888f046a1bb
bk-git-hub <bkprivate99@gmail.com>
2024-04-11 14:11:30 +0900

    chore: 디버깅용 콘솔로그 지우기

--- src/utils/getTimeDifference.js
+++ src/utils/getTimeDifference.js
@@ -5,19 +5,16 @@ const MILLISECONDS_IN_HOUR = 3600000;
 const MILLISECONDS_IN_MINUTE = 60000;
 
 const INTERVALS = [
-  { label: "년", divisor: MILLISECONDS_IN_YEAR },
-  { label: "달", divisor: MILLISECONDS_IN_MONTH },
-  { label: "일", divisor: MILLISECONDS_IN_DAY },
-  { label: "시간", divisor: MILLISECONDS_IN_HOUR },
-  { label: "분", divisor: MILLISECONDS_IN_MINUTE },
+  { label: '년', divisor: MILLISECONDS_IN_YEAR },
+  { label: '달', divisor: MILLISECONDS_IN_MONTH },
+  { label: '일', divisor: MILLISECONDS_IN_DAY },
+  { label: '시간', divisor: MILLISECONDS_IN_HOUR },
+  { label: '분', divisor: MILLISECONDS_IN_MINUTE },
 ];
 
 export default function getTimeDifference(createdDate, intervals = INTERVALS) {
   const currentDate = new Date();
   const timeDifference = currentDate - createdDate;
-
-  console.log(currentDate);
-  console.log(timeDifference);
 
   for (const interval of intervals) {
     const value = Math.floor(timeDifference / interval.divisor);
@@ -26,5 +23,5 @@ export default function getTimeDifference(createdDate, intervals = INTERVALS) {
     }
   }
 
-  return "방금 전";
+  return '방금 전';
 }
