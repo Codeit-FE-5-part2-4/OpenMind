361b32e622719126abc85ff2cd0e90d58e607e96
kuum97 <kuum97@icloud.com>
2024-04-11 16:53:10 +0900

    Merge: 링크페이지 병합 후 스타일 수정

--- src/pages/QuestionCardListPage/index.jsx
+++ src/pages/QuestionCardListPage/index.jsx
@@ -3,15 +3,23 @@ import "../../assets/styles/reset.css";
 import "../../assets/styles/global.css";
 import Pagination from "../../components/Pagination/Pagination";
 import styles from "./QuestionCardListPage.module.css";
+import SortList from "../../components/SortList/SortList";
+import ListHeader from "../../components/ListHeader/ListHeader";
 
 function QuestionCardListPage() {
   return (
-    <section className={styles.listPageContainer}>
-      <div>
-        <button>정렬</button>
+    <section className={styles.pageContainer}>
+      <div className={styles.contentContainer}>
+        <ListHeader />
+        <div className={styles.titleAndSortBox}>
+          <h1 className={styles.title}>누구에게 질문할까요?</h1>
+          <SortList />
+        </div>
+        <div className={styles.listAndPaginationBox}>
+          <QuestionCardList />
+          <Pagination />
+        </div>
       </div>
-      <QuestionCardList />
-      <Pagination />
     </section>
   );
 }
