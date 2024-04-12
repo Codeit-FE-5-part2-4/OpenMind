45483ac12b528aa1e35f662c4aa7f6b09665fe02
Kimhansaem <khs_38@naver.com>
2024-04-12 17:46:59 +0900

    Fead: Main페이지 기능 추가

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -3,20 +3,58 @@ import styles from "./Main.module.css";
 import logoImg from "../../assets/images/logo.png";
 import NameInput from "../../components/NameInput/NameInput";
 import MainHeader from "../../components/MainHeader/MainHeader";
+import { useCallback, useState } from "react";
+import { postUserInfo } from "../../api/nameApi";
+import { useNavigate } from "react-router-dom";
 
 function Main() {
-    return (
-        <main className={styles.mainWrap}>
-            <MainHeader />
-            <h1 className={styles.logo}>
-                <img src={logoImg} alt="" />
-            </h1>
-            <div className={styles.mainConBox}>
-                <NameInput placeholder={"이름을 입력하세요"} />
-                <BoxButton text={"질문 받기"} />
-            </div>
-        </main>
-    );
+  const [value, setValue] = useState("");
+  const navigate = useNavigate();
+
+  //button 이벤트
+  const handleNameSubmit = useCallback(async () => {
+    try {
+      const nameData = await postUserInfo(value);
+      if (!nameData || !nameData.id) {
+        throw new Error("Invalid data format");
+      }
+      const { id } = nameData;
+      navigate(`post/${id}/answer`);
+    } catch (e) {
+      alert(e.message);
+    }
+  }, [navigate, value]);
+
+  // 인풋 변경
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  //서브밋
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    handleNameSubmit();
+    setValue("");
+  };
+
+  return (
+    <main className={styles.mainWrap}>
+      <MainHeader />
+      <h1 className={styles.logo}>
+        <img src={logoImg} alt="" />
+      </h1>
+      <div className={styles.mainConBox}>
+        <form onSubmit={onSubmitForm}>
+          <NameInput
+            placeholder={"이름을 입력하세요"}
+            onChangeInput={onChangeInput}
+            value={value}
+          />
+          <BoxButton text={"질문 받기"} onClick={handleNameSubmit} />
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default Main;
