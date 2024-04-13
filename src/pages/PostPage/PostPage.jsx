8d52ecc6ae7a43be9422f197583ca24c85e532f2
bk-git-hub <bkprivate99@gmail.com>
2024-04-13 15:14:10 +0900

    refactor: useCallback 및 디펜던시 설정

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import logoImage from "../../assets/images/logo.png";
 import PostProfile from "../../components/PostProfile/PostProfile";
 import QuestionFeedList from "../../components/QuestionFeedList/QuestionFeedList";
@@ -15,28 +15,28 @@ export default function PostPage({ id }) {
   const [userProfile, setUserProfile] = useState({});
   const [userQuestions, setUserQuestions] = useState([]);
 
-  const fetchDataAndSetUserProfile = async () => {
+  const fetchDataAndSetUserProfile = useCallback(async () => {
     try {
       let result = await getSubjectInfo(id);
       setUserProfile(result);
     } catch (error) {
       console.error("피드 페이지를 가져오는 중에 오류가 발생했습니다:");
     }
-  };
+  }, [id]);
 
-  const fetchDataAndSetUserQuestions = async () => {
+  const fetchDataAndSetUserQuestions = useCallback(async () => {
     try {
       let result = await getSubjectQuestion(id);
       setUserQuestions(result);
     } catch (error) {
       console.error("질문 목록을 가져오는 중에 오류가 발생했습니다:");
     }
-  };
+  }, [id]);
 
-  const handleLoad = async () => {
+  const handleLoad = useCallback(async () => {
     await fetchDataAndSetUserProfile(id);
     await fetchDataAndSetUserQuestions(id);
-  };
+  }, [fetchDataAndSetUserProfile, fetchDataAndSetUserQuestions, id]);
 
   const handleModalOpen = () => {
     setShowModal(true);
@@ -58,7 +58,7 @@ export default function PostPage({ id }) {
     handleResize();
 
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [handleLoad, fetchDataAndSetUserProfile, fetchDataAndSetUserQuestions]);
 
   return (
     <div className={styles.container}>
