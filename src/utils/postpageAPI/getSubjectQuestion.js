7fe96f72fbbad30d64a8dfde4e9a979a2ef348a7
bk-git-hub <bkprivate99@gmail.com>
2024-04-13 14:17:10 +0900

    feat: api함수 에러 발생시 예외처리 추가

--- src/utils/postpageAPI/getSubjectQuestion.js
+++ src/utils/postpageAPI/getSubjectQuestion.js
@@ -5,6 +5,9 @@ export default async function getSubjectQuestion(id) {
   const response = await fetch(
     `${BASE_URL}${SUBJECTS_PATH}/${id}/${QEUSTIONS_PATH}`
   );
+  if (!response.ok) {
+    throw new Error("질문 목록 조회를 실패했습니다!");
+  }
   const result = await response.json();
   return result.results;
 }
