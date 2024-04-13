620e15be30731b8d0ef5e81be688f3f8624e7fe9
hakyoung12 <gkr097@naver.com>
2024-04-13 14:36:40 +0900

    feat: ArrowButton 페이지 이동 기능 구현

--- src/components/ArrowButton/ArrowButton.jsx
+++ src/components/ArrowButton/ArrowButton.jsx
@@ -1,9 +1,25 @@
 import arrowIcon from "../../assets/images/icon/arrow-right.svg";
 import styles from "./ArrowButton.module.css";
+import { useNavigate } from "react-router-dom";
 
 function ArrowButton({ text }) {
+  const navigate = useNavigate();
+
+  /** props로 받은 text를 이용하여 클릭시 이동할 페이지 결정(따로 컴포넌트로 뺄지 고민) */
+  const handleNavigate = () => {
+    const id = localStorage.getItem("id");
+    switch (text) {
+      case "질문하러 가기":
+        navigate("/list");
+        localStorage.setItem("id", 2221);
+        break;
+      case "답변하러가기":
+        id === null ? navigate("/") : navigate(`/post/${id}/answer`);
+        break;
+    }
+  };
   return (
-    <button className={styles.ArrowButton}>
+    <button className={styles.ArrowButton} onClick={handleNavigate}>
       {text} <img src={arrowIcon} alt="" />
     </button>
   );
