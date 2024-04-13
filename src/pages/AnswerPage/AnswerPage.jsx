98b89eae94ae8ee625cef8efacffde6b723f6cc8
bk-git-hub <bkprivate99@gmail.com>
2024-04-13 16:11:44 +0900

    refactor: useParams를 통한 라우터 경로 설정

--- src/pages/AnswerPage/AnswerPage.jsx
+++ src/pages/AnswerPage/AnswerPage.jsx
@@ -1,11 +1,34 @@
+import React, { useState, useEffect } from "react";
 import styles from "../PostPage/PostPage.module.css";
 import logoImage from "../../assets/images/logo.png";
 import PostProfile from "../../components/PostProfile/PostProfile";
 import QuestionFeedList from "../../components/QuestionFeedList/QuestionFeedList";
-import { mockProfile, mockQuestions } from "../PostPage/mockdata";
 import FloatingButton from "../../components/FloatingButton/FloatingButton";
+import getSubjectInfo from "../../utils/postpageAPI/getSubjectInfo";
+import getSubjectQuestion from "../../utils/postpageAPI/getSubjectQuestion";
+import { useParams } from "react-router-dom";
 
-export default function AnswerPage(params) {
+export default function AnswerPage() {
+  const { id } = useParams();
+  const [userProfile, setUserProfile] = useState({});
+  const [userQuestions, setUserQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const userProfileData = await getSubjectInfo(id);
+        setUserProfile(userProfileData);
+
+        const userQuestionsData = await getSubjectQuestion(id);
+        setUserQuestions(userQuestionsData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData(); // Call the fetchData function when the component mounts
+  }, [id]);
+
   return (
     <div className={styles.container}>
       <div className={styles.background}></div>
@@ -13,14 +36,14 @@ export default function AnswerPage(params) {
         <span className={styles.blind}>Openmind</span>
         <img className={styles.logoImage} src={logoImage} alt="openmind" />
       </h1>
-      <PostProfile userProfile={mockProfile} />
+      <PostProfile userProfile={userProfile} />
       <div className={styles.QuestionFeedContainer}>
         <div className={styles.FloatingButtonAtRightSide}>
           <FloatingButton text="삭제하기" />
         </div>
         <QuestionFeedList
-          questions={mockQuestions}
-          AnswererProfile={mockProfile}
+          questions={userQuestions}
+          AnswererProfile={userProfile}
           isAnswerPage={true}
         />
       </div>
