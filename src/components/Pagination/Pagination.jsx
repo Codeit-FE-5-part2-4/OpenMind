c6739087a64997526d7411f019a4eaecee08c2a5
hakyoung12 <gkr097@naver.com>
2024-04-16 20:07:58 +0900

    충돌해결

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,9 +1,25 @@
+import { type } from "@testing-library/user-event/dist/type";
 import styles from "./Pagination.module.css";
+import { useState } from "react";
 
 // 번호를 눌렀을 때 엑티브 클래스 추가하는 로직 추후에 구현
 
-function Pagination({ onArrow, totalPages, onNumber }) {
-  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+function Pagination({ onArrow, totalPages, onNumber, currentPageNumber }) {
+  const start =
+    currentPageNumber <= 3 //초기페이지 렌더링 조건
+      ? Math.max(2, currentPageNumber - 4)
+      : currentPageNumber < totalPages - 3 //마지막페이지 렌더링 조건
+      ? Math.max(2, currentPageNumber - 1)
+      : totalPages - 4; //중간페이지 렌더링
+
+  const last =
+    currentPageNumber <= 3 //초기페이지 렌더링 조건
+      ? Math.min(totalPages, currentPageNumber + (6 - currentPageNumber))
+      : currentPageNumber < totalPages - 3 //마지막페이지 렌더링 조건
+      ? Math.min(totalPages, currentPageNumber + 2)
+      : Math.min(totalPages, currentPageNumber + 3); //중간페이지 렌더링
+
+  const pageNumbers = Array.from({ length: last - start }, (_, i) => start + i);
 
   return (
     <section className={styles.paginationWrapper}>
@@ -15,16 +31,50 @@ function Pagination({ onArrow, totalPages, onNumber }) {
         &lt;
       </button>
       <ol className={styles.paginationNumbers}>
+        <li key={1}>
+          <button
+            className={
+              currentPageNumber === 1
+                ? styles.paginationButton__active
+                : styles.paginationButton
+            }
+            onClick={() => onNumber(1)}
+          >
+            1
+          </button>
+        </li>
+        {currentPageNumber > 3 && (
+          <li className={styles.paginationButton}>...</li>
+        )}
         {pageNumbers.map((number) => (
           <li key={number}>
             <button
-              className={`${styles.paginationButton} ${styles.active}`}
+              className={
+                currentPageNumber === number
+                  ? styles.paginationButton__active
+                  : styles.paginationButton
+              }
               onClick={() => onNumber(number)}
             >
               {number}
             </button>
           </li>
         ))}
+        {currentPageNumber < totalPages - 3 && (
+          <li className={styles.paginationButton}>...</li>
+        )}
+        <li key={totalPages}>
+          <button
+            className={
+              currentPageNumber === totalPages
+                ? styles.paginationButton__active
+                : styles.paginationButton
+            }
+            onClick={() => onNumber(totalPages)}
+          >
+            {totalPages}
+          </button>
+        </li>
       </ol>
       <button
         name="next"
