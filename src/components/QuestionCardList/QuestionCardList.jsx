a6d5f86ef3454a8169d700f6e3e34b1a822e95a3
hakyoung12 <gkr097@naver.com>
2024-04-17 21:22:36 +0900

    chore: 질문리스트페이지 질문카드 없을 시 스타일 추가

--- src/components/QuestionCardList/QuestionCardList.jsx
+++ src/components/QuestionCardList/QuestionCardList.jsx
@@ -1,8 +1,9 @@
 import QuestionCard from "../QuestionCard/QuestionCard";
 import styles from "./QuestionCardList.module.css";
+import ArrowButton from "../ArrowButton/ArrowButton";
 
 function QuestionCardList({ sortedFeeds }) {
-  return (
+  return sortedFeeds && sortedFeeds.length > 0 ? (
     <div className={styles.questionCard_wrapper}>
       <ul className={styles.listContainer}>
         {sortedFeeds?.map((feed, id) => (
@@ -11,6 +12,13 @@ function QuestionCardList({ sortedFeeds }) {
           </li>
         ))}
       </ul>
+    </div>
+  ) : (
+    <div className={styles.NotFoundPage_wrapper}>
+      <div className={styles.NotFoundPage_header}>
+        <h1>페이지를 찾을 수 없습니다</h1>
+        <ArrowButton text="질문하러 가기" />
+      </div>
     </div>
   );
 }
