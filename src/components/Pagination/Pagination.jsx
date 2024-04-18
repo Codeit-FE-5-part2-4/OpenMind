ad11dc184830c9a654015079f9580d016c21ffa4
kuum97 <kuum97@icloud.com>
2024-04-18 21:45:15 +0900

    Repactor: 리스트페이지의 핸들러마다 데이터페칭함수를 호출하던 것을 쿼리파라미터 상태로 최적화

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,82 +1,48 @@
-import { type } from "@testing-library/user-event/dist/type";
+import { useSearchParams } from "react-router-dom";
 import styles from "./Pagination.module.css";
-import classNames from "classnames";
+import { useEffect, useState } from "react";
 
-// 번호를 눌렀을 때 엑티브 클래스 추가하는 로직 추후에 구현
+function Pagination({ onArrow, onPage, currentPage, count }) {
+  const [totalPage, setTotalPage] = useState();
+  const [numbers, setNumbers] = useState([]);
+  const [searchParams] = useSearchParams();
 
-function Pagination({ onArrow, totalPages, onNumber, currentPageNumber }) {
-  const start = (() => {
-    if (currentPageNumber <= 3) {
-      //초기페이지 렌더링 조건
-      return 2; //초기페이지 렌더링
-    } else if (currentPageNumber < totalPages - 3) {
-      //중간페이지 렌더링 조건
-      return currentPageNumber - 1; //중간페이지 렌더링
-    } else return totalPages - 4; //마지막페이지 렌더링
-  })();
-
-  const last = (() => {
-    if (currentPageNumber <= 3) {
-      //초기페이지 렌더링 조건
-      return 6; //초기페이지 렌더링
-    } else if (currentPageNumber < totalPages - 3) {
-      //중간페이지 렌더링 조건
-      return currentPageNumber + 2; //중간페이지 렌더링
-    } else return totalPages; //마지막페이지 렌더링
-  })();
-
-  /** 시작페이지, 마지막페이지 제외하고 렌더링 */
-  const pageNumbers = Array.from({ length: last - start }, (_, i) => start + i);
-
-  /** 현재페이지에 active 클래스 추가 */
-  const handlePaginationNumber = (number) => {
-    return classNames(styles.paginationButton, {
-      [styles.active]: currentPageNumber === number,
-    });
-  };
-
-  /** 페이지번호 렌더링 */
-  const pageRenderingNumber = (number) => {
-    return (
-      <li key={number}>
-        <button
-          className={handlePaginationNumber(number)}
-          onClick={() => {
-            onNumber(number);
-          }}
-        >
-          {number}
-        </button>
-      </li>
-    );
-  };
+  useEffect(() => {
+    try {
+      const totalPage = Math.ceil(count / 8);
+      const numbers = Array.from({ length: totalPage }, (_, i) => i + 1);
+      setTotalPage(totalPage);
+      setNumbers(numbers);
+    } catch (error) {
+      console.error("Error generating pages:", error);
+    }
+  }, [count, searchParams]);
 
   return (
     <section className={styles.paginationWrapper}>
       <button
-        name="previous"
         className={styles.paginationArrow}
-        onClick={(e) => onArrow(e)}
+        onClick={() => onArrow("previous")}
+        disabled={currentPage <= 1}
       >
         &lt;
       </button>
       <ol className={styles.paginationNumbers}>
-        {/* 1페이지 */}
-        {pageRenderingNumber(1)}
-        {currentPageNumber > 3 && (
-          <li className={styles.paginationButton}>...</li>
-        )}
-        {pageNumbers.map((number) => pageRenderingNumber(number))}
-        {currentPageNumber < totalPages - 3 && (
-          <li className={styles.paginationButton}>...</li>
-        )}
-        {/* 마지막페이지 */}
-        {pageRenderingNumber(totalPages)}
+        {numbers.map((number) => (
+          <li key={number}>
+            <button
+              className={styles.paginationButton}
+              onClick={() => onPage(number)}
+            >
+              {number}
+            </button>
+          </li>
+        ))}
       </ol>
       <button
-        name="next"
         className={styles.paginationArrow}
-        onClick={(e) => onArrow(e)}
+        onClick={() => onArrow("next")}
+        disabled={currentPage >= totalPage}
       >
         &gt;
       </button>
