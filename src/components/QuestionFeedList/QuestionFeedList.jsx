9cbdc2863ade15d49aaff2de7b533ca5e93e10ac
bk-git-hub <bkprivate99@gmail.com>
2024-04-19 16:19:27 +0900

    style: temporary animation

--- src/components/QuestionFeedList/QuestionFeedList.jsx
+++ src/components/QuestionFeedList/QuestionFeedList.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import speechBubble from "../../assets/images/icon/speechBubble.svg";
 import QuestionFeedCard from "../QuestionFeedCard/QuestionFeedCard";
 import styles from "./QuestionFeedList.module.css";
@@ -19,32 +20,42 @@ export default function QuestionFeedList({
     <div className={styles.container}>
       <div className={styles.questionCount}>
         <img src={speechBubble} alt="말풍선" />
-
         <span className={styles.questionCountMsg}>{questionCountMsg}</span>
       </div>
 
       {questions.length > 0 ? (
-        <ul className={styles.questionList}>
-          {questions.map((question) => {
-            return (
-              <li key={question.id}>
-                <QuestionFeedCard
-                  AnswererProfile={AnswererProfile}
-                  isAnswerPage={isAnswerPage}
-                  question={question}
-                  updateQuestions={updateQuestions}
-                  modalHandler={modalHandler}
-                />
-              </li>
-            );
-          })}
-        </ul>
+        <motion.ul
+          className={styles.questionList}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delayChildren: 0.3 }}
+        >
+          {questions.map((question, index) => (
+            <motion.li
+              key={question.id}
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+            >
+              <QuestionFeedCard
+                AnswererProfile={AnswererProfile}
+                isAnswerPage={isAnswerPage}
+                question={question}
+                updateQuestions={updateQuestions}
+                modalHandler={modalHandler}
+              />
+            </motion.li>
+          ))}
+        </motion.ul>
       ) : (
         <div className={styles.questionCardEmpty}>
-          <img
+          <motion.img
             className={styles.questionEmptyImage}
             src={emptyIcon}
             alt="질문없음"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.3 }}
           />
         </div>
       )}
